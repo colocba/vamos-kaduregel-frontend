073e1f70fda10a6eb0d20a869316e4a902a752f4
abaum-augu <abaum@augury.com>
2026-05-03 15:00:06 +0300

    chore: add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Vamos Kaduregel</h1>;
+  return (
+    <main className="flex min-h-full items-center justify-center p-4">
+      <h1 className="text-3xl font-bold">Vamos Kaduregel</h1>
+    </main>
+  );
 }
